af599b5784ac963a9e7c3fa9b81ea2fc94dca48f
ymanya <valleys10k@gmail.com>
2022-04-23 22:28:11 +0900

    ViewModelとCombine対応

--- GitHubSearchV3/View/Main/MainViewController.swift
+++ GitHubSearchV3/View/Main/MainViewController.swift
@@ -7,45 +7,18 @@
 //
 
 import UIKit
+import Combine
 import SafariServices
 
 // MARK: - ViewController
 class MainViewController: UIViewController {
   // MARK: - クラス内変数
-  /// 取得したリポジトリ配列
-  var repositories = [GitHubRepository]() {
-    didSet {
-      if repositories.isEmpty {
-        tableView.separatorStyle = .none
-        resultText = ""
-      } else {
-        tableView.separatorStyle = .singleLine
-      }
-      tableView.reloadData()
-    }
-  }
-  /// 結果件数表示文字列
-  var resultText = "" {
-    didSet {
-      resultLabel.text = resultText
-    }
-  }
-  /// インクリメンタルサーチのスロットリングを行うタイマー
-  var timer: Timer?
+  
+  var subscriptions = Set<AnyCancellable>()
+  let viewModel: MainViewModel = MainViewModel()
+  
   /// 検索バー
   let searchBar = UISearchBar()
-  
-  var searchText = ""
-  var page = 1
-  var isNextPageLoading = false {
-    didSet {
-      if self.isNextPageLoading {
-        footerActivityIndicator.startAnimating()
-      } else {
-        footerActivityIndicator.stopAnimating()
-      }
-    }
-  }
   
   // MARK: - IBOutlet
   @IBOutlet weak var tableView: UITableView!
@@ -56,7 +29,6 @@ class MainViewController: UIViewController {
   // MARK: - ViewController Lifecycle methods
   override func viewDidLoad() {
     super.viewDidLoad()
-    repositories = []
     
     searchBar.delegate = self
     searchBar.placeholder = "Search"
@@ -65,6 +37,70 @@ class MainViewController: UIViewController {
     searchBar.returnKeyType = .search
     searchBar.becomeFirstResponder()
     navigationItem.titleView = searchBar
+    
+    setupViewModel()
+  }
+  
+  func setupViewModel() {
+    viewModel.outputs.repositoriesSubject
+      .sink(receiveValue: { [weak self] _ in
+        DispatchQueue.main.async {
+          self?.tableView.reloadData()
+        }
+      })
+      .store(in: &subscriptions)
+    
+    viewModel.outputs.isLoadingSubject
+      .sink(receiveValue: { [weak self] isLoading in
+        DispatchQueue.main.async {
+          if (isLoading) {
+            self?.activityIndicator.startAnimating()
+          } else {
+            self?.activityIndicator.stopAnimating()
+          }
+        }
+      })
+      .store(in: &subscriptions)
+    
+    viewModel.outputs.isNextPageLoadingSubject
+      .sink(receiveValue: { [weak self] isNextPageLoading in
+        DispatchQueue.main.async {
+          if isNextPageLoading {
+            self?.footerActivityIndicator.startAnimating()
+          } else {
+            self?.footerActivityIndicator.stopAnimating()
+          }
+        }
+      })
+      .store(in: &subscriptions)
+    
+    viewModel.outputs.resultTextSubject
+      .sink(receiveValue: { [weak self] resultText in
+        DispatchQueue.main.async {
+          self?.resultLabel.text = resultText
+        }
+      })
+      .store(in: &subscriptions)
+    
+    viewModel.outputs.showWebViewSubject
+      .sink(receiveValue: { [weak self] url in
+        DispatchQueue.main.async {
+          let safariVC = SFSafariViewController(url: url)
+          safariVC.dismissButtonStyle = .close
+          self?.present(safariVC, animated: true, completion: nil)
+        }
+      })
+      .store(in: &subscriptions)
+    
+    viewModel.outputs.errorAlertSubject
+      .sink(receiveValue: { [weak self] errorAlert in
+        DispatchQueue.main.async {
+          let alert = UIAlertController(title: "Error", message: errorAlert, preferredStyle: .alert)
+          alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+          self?.present(alert, animated: true, completion: nil)
+        }
+      })
+      .store(in: &subscriptions)
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -90,41 +126,11 @@ extension MainViewController: UISearchBarDelegate {
   
   func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
     guard searchText.count > 0 else {
-      repositories = []
+      viewModel.inputs.resetSearchQuery()
       return
     }
     
-    activityIndicator.startAnimating()
-    resultText = ""
-    
-    self.searchText = searchText
-    page = 1
-    searchRepository()
-  }
-  
-  func searchRepository() {
-    timer?.invalidate()
-    timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false) { [weak self] _ in
-      guard let self = self else { return }
-      
-      Task {
-        do {
-          let result = try await GitHubClient.SearchRepositories(query: self.searchText, page: "\(self.page)").send()
-          
-          self.activityIndicator.stopAnimating()
-      
-          if self.isNextPageLoading {
-            self.repositories = self.repositories + result.items
-          } else {
-            self.repositories = result.items
-          }
-          self.resultText = "\(self.repositories.count) of \(result.totalCount)"
-          self.isNextPageLoading = false
-        } catch(let error) {
-          self.showAlert(withMessage: error.localizedDescription)
-        }
-      }
-    }
+    viewModel.inputs.searchRepository(query: searchText)
   }
   
   func showAlert(withMessage message: String) {
@@ -141,9 +147,7 @@ extension MainViewController: UISearchBarDelegate {
 // MARK: - TableView
 extension MainViewController: UITableViewDelegate {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let safariVC = SFSafariViewController(url: repositories[indexPath.row].htmlUrl)
-    safariVC.dismissButtonStyle = .close
-    present(safariVC, animated: true, completion: nil)
+    viewModel.inputs.handleDidSelectRowAt(indexPath)
   }
   
   func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
@@ -158,25 +162,23 @@ extension MainViewController: UITableViewDelegate {
       return
     }
     
-    guard !isNextPageLoading else {
+    guard !viewModel.isLoadingSubject.value && !viewModel.isNextPageLoadingSubject.value else {
       return
     }
     
     if distanceToBottom < 500 {
-      page = page + 1
-      isNextPageLoading = true
-      searchRepository()
+      viewModel.inputs.fetchNextPage()
     }
   }
 }
 
 extension MainViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return repositories.count
+    return viewModel.repositoriesSubject.value.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let repository = repositories[indexPath.row]
+    let repository = viewModel.repositoriesSubject.value[indexPath.row]
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! RepositoryTableViewCell
     cell.configure(with: repository)
     return cell
---
+++ GitHubSearchV3/View/Main/MainViewModel.swift
@@ -0,0 +1,121 @@
+//
+//  MainViewModel.swift
+//  GitHubSearchV3
+//
+//  Created by Yuki Manya.
+//  Copyright © ymanya. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+protocol MainViewModelInputs {
+  func searchRepository(query: String?)
+  func fetchNextPage()
+  func resetSearchQuery()
+  func handleDidSelectRowAt(_ indexPath: IndexPath)
+}
+
+protocol MainViewModelOutputs {
+  var repositoriesSubject: CurrentValueSubject<[GitHubRepository], Never> { get }
+  var isLoadingSubject: CurrentValueSubject<Bool, Never> { get }
+  var isNextPageLoadingSubject: CurrentValueSubject<Bool, Never> { get }
+  var resultTextSubject: PassthroughSubject<String, Never> { get }
+  var showWebViewSubject: PassthroughSubject<URL, Never> { get }
+  var errorAlertSubject: PassthroughSubject<String, Never> { get }
+}
+
+protocol MainViewModelType {
+  var inputs: MainViewModelInputs { get }
+  var outputs: MainViewModelOutputs { get }
+}
+
+final class MainViewModel: MainViewModelType, MainViewModelOutputs {
+  
+  var inputs: MainViewModelInputs { return self }
+  var outputs: MainViewModelOutputs { return self }
+  
+  var repositoriesSubject = CurrentValueSubject<[GitHubRepository], Never>([])
+  var isLoadingSubject = CurrentValueSubject<Bool, Never>(false)
+  var isNextPageLoadingSubject = CurrentValueSubject<Bool, Never>(false)
+  var resultTextSubject = PassthroughSubject<String, Never>()
+  var showWebViewSubject = PassthroughSubject<URL, Never>()
+  var errorAlertSubject = PassthroughSubject<String, Never>()
+  
+  /// 検索テキスト
+  var searchText: String? = ""
+  /// 現在表示中のページ
+  var page = 1
+  /// 最終ページに到達したかのフラグ
+  var isReachedLastPage = false
+  /// インクリメンタルサーチのスロットリングを行うタイマー
+  var timer: Timer?
+
+}
+
+extension MainViewModel: MainViewModelInputs {
+  
+  func searchRepository(query: String?) {
+    guard let query = query else { return }
+    
+    timer?.invalidate()
+    
+    isLoadingSubject.send(true)
+    page = 1
+    repositoriesSubject.send([])
+    searchText = query
+    resultTextSubject.send("")
+    
+    timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false) { _ in
+      Task { [weak self] in
+        do {
+          let result = try await GitHubClient.SearchRepositories(query: query, page: "\(self?.page ?? 1)").send()
+          
+          self?.isLoadingSubject.send(false)
+          self?.repositoriesSubject.value.append(contentsOf: result.items)
+          self?.resultTextSubject.send("\(result.items.count) of \(result.totalCount)")
+        } catch(let error) {
+          self?.isLoadingSubject.send(false)
+          guard let error = error as? GitHubClientError else { return }
+          self?.errorAlertSubject.send(error.localizedDescription)
+        }
+      }
+    }
+  }
+  
+  func fetchNextPage() {
+    guard !isReachedLastPage else { return }
+    
+    isNextPageLoadingSubject.send(true)
+    
+    page = page + 1
+    
+    Task {
+      do {
+        let result = try await GitHubClient.SearchRepositories(query: searchText ?? "", page: "\(page)").send()
+        
+        isNextPageLoadingSubject.send(false)
+        repositoriesSubject.value.append(contentsOf: result.items)
+        resultTextSubject.send("\(self.repositoriesSubject.value.count) of \(result.totalCount)")
+        isReachedLastPage = repositoriesSubject.value.count == result.totalCount
+      } catch(let error) {
+        isNextPageLoadingSubject.send(false)
+        guard let error = error as? GitHubClientError else { return }
+        errorAlertSubject.send(error.localizedDescription)
+      }
+    }
+  }
+
+  func resetSearchQuery() {
+    page = 1
+    repositoriesSubject.send([])
+    searchText = ""
+    resultTextSubject.send("")
+  }
+  
+  func handleDidSelectRowAt(_ indexPath: IndexPath) {
+    let item = repositoriesSubject.value[indexPath.row]
+    showWebViewSubject.send(item.htmlUrl)
+  }
+  
+}
